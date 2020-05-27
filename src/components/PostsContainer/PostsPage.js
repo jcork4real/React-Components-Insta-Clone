c8619bcd58ce5b9d0e4820fca3778244487b71a9
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2020-05-26 17:46:45 -0700

    MVP Reached

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,30 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 
-const PostsPage = () => {
+import dummyData from '../../dummy-data';
+
+const PostsPage = (props) => {
   // set up state for your data
+  const [data, setData] = useState(dummyData);
+
+  useEffect(() => {
+    const filteredData = dummyData.filter((item) => {
+      return item.username.includes(props.search);
+    });
+
+    setData(filteredData);
+  }, [props.search]);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map((post) => {
+        return <Post key={post.username} post={post} />;
+      })}
     </div>
   );
 };
