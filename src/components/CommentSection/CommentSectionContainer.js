1adfd870d1eda825c547aa3d1146bfbcae6fea05
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2020-05-26 18:31:12 -0700

    mvp

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,19 +1,20 @@
 // You will add code to this file
-import React from "react";
+import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
+  // Add state for the comments
+  console.log(props);
+  const [comments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      {props.comments.map((comment, idx) => {
-        return <Comment key={`${comment.username}-${idx}`} comment={comment} />
-      })}
-      <CommentInput setComments={props.setComments}
-                    comments={props.comments} />
+      {comments.map((comment, index) => {return <Comment comment={comment} key={index} />})}
+
+      <CommentInput />
     </div>
   );
 };
